7f79a77114bef05761dbb4ee4edb824c977d5913
OguzhanOguz <oguzkhans2001@gmail.com>
2023-03-10 19:27:54 +0300

    Create main.py
    Kamp ödevi 1

---
+++ main.py
@@ -0,0 +1,21 @@
+#<--Python veri tipleri-->
+
+#integer => tam sayılar(3,4,25)-->Sitede eğitimlerin tamamlanma yüzdesi
+#float => ondalıklı sayılar(2.5,-4.9,0.3)
+#string => metinsel ifadeler("Merhaba","Benim adim oguzhan","Gorusuruz")-->Kursların adları
+#complex => karmaşık sayılar(5j,-12j,j)
+#boolean => True ya da False-->giriler e-postanın daha önce girilip girilmediğini göstermesi
+#list => [] içerisine tanımladığımız sıralı veri tipleridir,içlerinde farklı veri tipleri bulundurabilirler,güncellenebilirler,tekrarlanabilirler(x = ['e','a','a','z'])-->kariyer sekmesindeki bilgilerimiz
+#tuple => () içerisine tanımladığımız sıralı veri tipleridir,içlerinde farklı veri tipleri bulundurabilirler,güncellenemezler,tekrarlanabilirler(y = ('elma','armut','muz'))
+#range => belirli bir aralıktaki sayıları sıralar((range(5) = {0,1,2,3,4}),(range(3,6) = {3,4,5}))
+#dictionary(dict) => sözlük yapısı {} ile tanımlanır.Bir kelime ya da harfe karşılık gelen başka bir kelimeyi ifade eder.(kisi={"isim":"Oguzhan","Bolum":"Bilgisayar muhendisligi"})-->Sitedeki kullanıcı adımız ve şifremiz
+#set => {} içerisine tanımladığımız sırasız veri tipleridir,içlerinde farklı veri tipleri bulundurabilirler,güncellenebilirler,tekrarlanamazlar(S = {20,'elma',3.5})-->Sıkça sorulan sorular kısmı
+#frozenset => {} içerisine tanımladığımız sırasız veri tipleridir,içlerinde farklı veri tipleri bulundurabilirler,güncellenemezler,tekrarlanamazlar(K = {33,'mersin',-35.7})
+
+
+#<--Kodlama.io sitesinde şart blokları kullanıldığını düşündüğüm kısım-->
+
+if(enteredEmail==email)
+   print("Giris yapildi")
+else 
+   print("Lutfen tekrar deneyiniz")
